32e1087f8e0c1d708c7101dfd2dcb381e2e8a605
sghosh89 <ghoshshyamolina89@gmail.com>
2019-03-27 15:16:56 -0500

    pedagog fig chunk added

---
+++ ExtremeTailDep.R
@@ -0,0 +1,40 @@
+#Generates data with extreme tail dependence. Function name
+#stands for "extreme tail dependence".
+#
+#Args
+#n        Number of points to get
+#d        Dimension
+#rl       1 for right tail dependence, -1 for left tail dependence
+#mn       mean and 
+#sdev     standard deviation
+#
+#Output
+#A matrix of dimension n by d, marginals are standard normal.
+#
+retd<-function(n,d,rl,mn,sdev)
+{
+  m<-matrix(rnorm(n*(d+1),mean=mn,sd=sdev),n,d+1)
+  flag<-(m[,1]>0) #a coin toss
+  m<-m[,2:(d+1)] #an n by d matrix of iid standard normals
+  
+  m[flag,]<-rep(abs(m[flag,1]),times=d)
+  m[!flag,]<-(-abs(m[!flag,]))
+  
+  if(rl==-1)
+  {
+    m<-(-m)  
+  }
+  
+  return(m)
+}
+
+##test
+#d<-retd(10000,2,1)
+#plot(d[,1],d[,2],type='p')
+#hist(d[,1],50)
+#hist(d[,2],50)
+#
+#d<-retd(10000,2,-1)
+#plot(d[,1],d[,2],type='p')
+#hist(d[,1],50)
+#hist(d[,2],50)
