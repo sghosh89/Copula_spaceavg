9b3f55200c46306ce2d7d798df292b53f92b7b48
Dan Reuman <reuman@ku.edu>
2019-04-17 12:07:05 -0500

    still working, have not suceeded yet in making decent surrogates

--- pps2_exist_JRG_CommonAndPseudo.R
+++ pps2_exist_JRG_CommonAndPseudo.R
@@ -37,6 +37,7 @@ numevalsrough<-250
 resloc<-"./Results/Results_pps2_exist_JRG_CommonAndPseudo/"
 theseed1<-103
 theseed2<-1001
+theseed3<-203
 
 numpd<-10000 #find at least this many positive definite matrices of parameters before stopping
 
@@ -63,7 +64,7 @@ if (!dir.exists(paste0(resloc,"partmap")))
   dir.create(paste0(resloc,"partmap"))
 }
 
-#***For each pair of species, find the preimage of their corelation 
+#***For each pair of species, find the preimage of their covariance 
 #under the map defined by copmap.R
 
 set.seed(theseed1)
@@ -87,11 +88,11 @@ for (c1 in 1:(numsp-1))
     #get the (stochastic) map
     thisres<-copmap(x,y,prough,numevalsrough)
     
-    #find the iverse range
-    allprg[c1,c2,c(1,3)]<-getinvrg(prough,thisres,cor(x,y))
+    #find the inverse range
+    allprg[c1,c2,c(1,3)]<-getinvrg(prough,thisres,cov(x,y))
     
     #find the actual preimage
-    allprg[c1,c2,2]<-getinv(prough,thisres,cor(x,y))
+    allprg[c1,c2,2]<-getinv(prough,thisres,cov(x,y))
     
     #make and save a plot
     mn<-apply(FUN=mean,MARGIN=2,X=thisres)
@@ -101,7 +102,7 @@ for (c1 in 1:(numsp-1))
     lines(prough,quants[2,],type='l',lty="dashed")
     lines(prough,quants[1,],type='l',lty="dotted")
     lines(prough,quants[3,],type='l',lty="dotted")
-    lines(range(prough),rep(cor(x,y),2))
+    lines(range(prough),rep(cov(x,y),2))
     lines(rep(allprg[c1,c2,1],2),c(-1,1))
     lines(rep(allprg[c1,c2,2],2),c(-1,1))
     lines(rep(allprg[c1,c2,3],2),c(-1,1))
@@ -127,7 +128,7 @@ sum(m1==m2)
 prod(dim(m1))
 is.positive.semi.definite(m1)
 eigen(m1)$values
-#If m1 is pos semi def, we can skip some of the next steps
+#If m1 is pos semi def, we can skip some or all of the next steps
 
 #now use nearPD and see if you get a result between hlo and hhi
 pvlo<-P2p(t(allprg[,,1])) #pv stands for p in vector format
@@ -148,6 +149,8 @@ cbind(pvlo[inds],pvNPD[inds],pvhi[inds])
 #(deterministic) function, and that function evaluated on allprg[,,1] 
 #and allprg[,,3] (which will be used later for bounds for an optimization).
 
+set.seed(theseed2)
+
 #loop through each pair of species
 allcoefs<-array(NA,c(numsp,numsp,3))
 vbds<-array(NA,c(numsp,numsp,2))
@@ -161,7 +164,7 @@ for (c1 in 1:(numsp-1))
     y<-d[,c2]
     
     #get the (stochastic) map
-    pthis<-seq(from=allprg[c1,c2,1],to=allprg[c1,c2,3],by=0.01)
+    pthis<-seq(from=allprg[c1,c2,1],to=allprg[c1,c2,3],length.out=30)
     thisres<-copmap(x,y,pthis,numevalsfine)
     
     #do a regression
@@ -191,40 +194,135 @@ for (c1 in 1:(numsp-1))
 
 #***Now do the optimization
 
-set.seed(theseed2)
+set.seed(theseed3)
 
-allposdef<-matrix(NA,numpd,length(pvlo)+1)
+allresults<-matrix(NA,numpd,2*length(pvlo)+1)
 rowcount<-1
 
+covd<-cov(d)
+vtotx<-sum(covd)
+sumvarsx<-sum(diag(covd))
 
-#This function returns a minimum eigenvalue and needs to be maximized
-fn<-function(vpart)
+#For computing the inverse of the fitted expectation of copmap
+#
+#Args
+#threecoefs       The coefficients of the map
+#y                Take the inverse of this point under the map
+#ylimits, xlimits   Should be the most extreme values
+#
+#Output: a single number
+invval<-function(threecoefs,y,ylimits,xlimits)
 {
-  #get the remaining coordinate of v - the vs are correlations
-    #convert to covariances
-    #
-  
-  #if that coordinate is out of the acceptable range, return -Inf
-  
-  
-  #calculate the inverses to get p
-  
-  
-  #calculate the minimum eigenvalue
-  res<-min(eigen(p2P(p))$values)
-  if (res>0)
+  c<-threecoefs[1]
+  b<-threecoefs[2]
+  a<-threecoefs[3]
+
+  #some checks
+  h<-a*xlimits^2+b*xlimits+c
+  if (!isTRUE(all.equal(h,ylimits)))
   {
-    allposdef[rowcount,]<<-c(p,res)
-    rowcount<<-rowcount+1 #note the use of global variables, watch out!
-    print("Found one!")
+    stop("Error in invval: inconsistent arguments")
   }
+  #maybe later add a check that the deriv of the function is positive everywhere inside xlimits?
+  #if (y<ylimits[1] || y>ylimits[2])
+  #{
+  #  print(paste0("y: ",y,"; ylimits[1]: ",ylimits[1],"; ylimits[2]: ",ylimits[2]))
+  #  stop("Error in invval: out of range y argument")
+  #}
+  
+  c<-(c-y)
+  if (isTRUE(all.equal(a,0)))
+  {
+    res<-(-c/b)
+  }else
+  {
+    res<-(-b+sqrt(b^2-4*a*c))/(2*a)    
+  }
+  
+  #more checks
+  #if (res<xlimits[1] || res>xlimits[2])
+  #{
+  #  stop("Error in invval: res not in range")
+  #}
   
   return(res)
 }
 
+#This function returns a minimum eigenvalue and needs to be maximized
+fn<-function(vpart)
+{
+  #get the remaining coordinate of v - the vs are covariances
+  v12<-(vtotx-2*sum(vpart)-sumvarsx)/2
+  
+  #if that coordinate is out of the acceptable range, return a low value - can't return
+  #-Inf because the optimizer I used needs finite values
+  if (v12<vbds[1,2,1] || v12>vbds[1,2,2])
+  {
+    return(-9)
+  }
+  v<-p2P(c(v12,vpart))
+  
+  #calculate the inverses to get p
+  p<-matrix(NA,numsp,numsp)
+  for (c1 in 1:(numsp-1))
+  {
+    for (c2 in (c1+1):numsp)
+    {
+      #print(paste0("c1: ",c1,"; c2: ",c2))
+      p[c1,c2]<-invval(threecoefs=allcoefs[c1,c2,],y=v[c1,c2],
+                       ylimits=vbds[c1,c2,],xlimits=allprg[c1,c2,c(1,3)])
+    }
+  }
+  p[is.na(p)]<-0
+  p<-p+t(p)
+  diag(p)<-1
+  
+  #calculate the minimum eigenvalue
+  res<-min(eigen(p)$values)
+  if (res>0)
+  {
+    allresults[rowcount,]<<-c(P2p(p),P2p(v),res)
+    rowcount<<-rowcount+1 #note the use of global variables, watch out!
+    print("Found one!")
+  }
+  
+  #print(res)
+  return(res)
+}
 
+vparttest<-P2p(covd)
+vparttest<-vparttest[2:length(vparttest)]
+fn(vparttest)
+fn(vparttest+c(-0.01,rep(0,length(vparttest)-1))) #just to make sure it changes
+fn(vparttest+c(0.01,rep(0,length(vparttest)-1))) 
 
+#do a search starting with covariances of the actual data
+vpart<-P2p(covd)
+vpart<-vpart[2:length(vpart)]
+lower<-P2p(t(vbds[,,1]))
+lower<-lower[2:length(lower)]
+upper<-P2p(t(vbds[,,2]))
+upper<-upper[2:length(upper)]
+optres<-optim(vpart,fn,method="L-BFGS-B",lower=lower,upper=upper,
+      control=list(fnscale=-1)) 
 
+#just eval fn at a bunch of points
+lower10<-(99/100)*vpart+(1/100)*lower
+upper10<-(99/100)*vpart+(1/100)*upper
+for (counter in 1:100)
+{
+  startv<-(upper10-lower10)*runif(length(lower10))+lower10
+  print(fn(startv))
+}
+#we are getting almost all values that don't work!
 
+hist(log10(upper-lower)) #this is terrible, no wonder the optimizer does not work.
+#Makes me think I should go back to cor.
 
-
+#do some searches from nearby starts
+lower10<-(9/10)*vpart+(1/10)*lower
+upper10<-(9/10)*vpart+(1/10)*upper
+start1<-(upper10-lower10)*runif(length(lower10))+lower10
+fn(start1)
+optres<-optim(start1,fn,method="L-BFGS-B",lower=lower,upper=upper,
+              control=list(fnscale=-1)) 
