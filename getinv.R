b013d81ff94830c10866c02271ee64ebda489f56
Dan Reuman <reuman@ku.edu>
2019-04-18 15:27:46 -0500

    added an option to getinv on whether to use mean or median

--- getinv.R
+++ getinv.R
@@ -8,6 +8,8 @@
 #imval        The image value to take the preimage of - would be the correlation 
 #               or covariance of the x and y inputs to copmap, depending on the 
 #               value of cflag that was used in the call to copmap
+#center       Either "mean" or "median" depending on what you want the preimage 
+#               of
 #
 #Output
 #Taking the means of each column of copmapout gives a vector of the same length 
@@ -16,11 +18,22 @@
 #If there is no point in the pre-image, the function returns -Inf. If more than 
 #one point, it returns Inf. If exactly one point, it returns that value.
 
-getinv<-function(p,copmapout,imval)
+getinv<-function(p,copmapout,imval,center="mean")
 {
   #x and y of the function to be inverted
   x<-p
-  y<-apply(FUN=mean,X=copmapout,MARGIN=2)
+  if (center=="mean")
+  {
+    y<-apply(FUN=mean,X=copmapout,MARGIN=2)
+  }
+  if (center="median")
+  {
+    y<-apply(FUN=median,X=copmapout,MARGIN=2)
+  }
+  if (!(center %in% c("mean","median")))
+  {
+    stop("Error in getinv: bad value for center")
+  }
   
   res<-numeric(0)
   for (counter in 1:(length(x)-1))
