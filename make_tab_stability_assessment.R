d14765ae2ee85e0115ab1816323664758ffb2eaf
sghosh89 <ghoshshyamolina89@gmail.com>
2019-04-30 12:12:23 -0500

    modified for knz

--- make_tab_stability_assessment.R
+++ make_tab_stability_assessment.R
@@ -89,122 +89,123 @@ make_tab_stability<-function(m,surrogs,surrogs_given){
 ### Bring in the data
 
 #Importing the two dataset matrices of years by species
-d_jrg<- readRDS("./Results/jrg_results/skewness_results/ts_mat_all_sp_39_jrg.RDS")# for all species except "BARE","ROCK","LASP"
-d_hays<- readRDS("./Results/hays_results/skewness_results/ts_mat_all_sp_146_hays.RDS")# for all species except in "REMOVE" category
+#d_jrg<- readRDS("./Results/jrg_results/skewness_results/ts_mat_all_sp_39_jrg.RDS")# for all species except "BARE","ROCK","LASP"
 
 #Importing data with "C", "I", "R" values for common, intermediate, and rare species
-category_jrg<-readRDS("./Results/jrg_results/skewness_results/all_sp_39_jrg_category.RDS")
-category_hays<-readRDS("./Results/hays_results/skewness_results/all_sp_146_hays_category.RDS")  
+#category_jrg<-readRDS("./Results/jrg_results/skewness_results/all_sp_39_jrg_category.RDS")
 
-# Now call for jrg data
-m<-d_jrg
-id_sp<-which(category_jrg$category %in% "C")
-m<-m[,id_sp]
-jrg_C<-make_tab_stability(m=m,surrogs = NA,surrogs_given = F)
+#================================= Now call for jrg data ===================================================
+#m<-d_jrg
+#id_sp<-which(category_jrg$category %in% "C")
+#m<-m[,id_sp]
+#jrg_C<-make_tab_stability(m=m,surrogs = NA,surrogs_given = F)
 
-m<-d_jrg
-id_sp<-which(category_jrg$category %in% c("C","I"))
-m<-m[,id_sp]
-jrg_CI<-make_tab_stability(m=m,surrogs = NA,surrogs_given = F)
+#m<-d_jrg
+#id_sp<-which(category_jrg$category %in% c("C","I"))
+#m<-m[,id_sp]
+#jrg_CI<-make_tab_stability(m=m,surrogs = NA,surrogs_given = F)
 
-m<-d_jrg
-id_sp<-which(category_jrg$category %in% c("C","I","R"))
-m<-m[,id_sp]
-jrg_CIR<-make_tab_stability(m=m,surrogs = NA,surrogs_given = F)
+#m<-d_jrg
+#id_sp<-which(category_jrg$category %in% c("C","I","R"))
+#m<-m[,id_sp]
+#jrg_CIR<-make_tab_stability(m=m,surrogs = NA,surrogs_given = F)
 
-jrg_stability<-rbind(jrg_C,jrg_CI,jrg_CIR)
-rownames(jrg_stability)<-c("C","C+I","all")
+#jrg_stability<-rbind(jrg_C,jrg_CI,jrg_CIR)
+#rownames(jrg_stability)<-c("C","C+I","all")
 
-write.csv(jrg_stability,"./Results/jrg_results/skewness_results/jrg_stability_C_CI_CIR.csv")
+#write.csv(jrg_stability,"./Results/jrg_results/skewness_results/jrg_stability_C_CI_CIR.csv")
 
 #--------------------------------------------------------
 # Now for pearson preserving surrogates with C+pseudo sp.
-m<-d_jrg
-id_sp<-which(category_jrg$category %in% "C")
-m<-m[,id_sp] # for common sp.
-id_sp<-which(!category_jrg$category %in% "C")
-m_pseudo<-d_jrg[,id_sp]
-pseudo_sp<-apply(m_pseudo,MARGIN = 1,FUN = sum)
-m<-cbind(m,pseudo_sp)
+#m<-d_jrg
+#id_sp<-which(category_jrg$category %in% "C")
+#m<-m[,id_sp] # for common sp.
+#id_sp<-which(!category_jrg$category %in% "C")
+#m_pseudo<-d_jrg[,id_sp]
+#pseudo_sp<-apply(m_pseudo,MARGIN = 1,FUN = sum)
+#m<-cbind(m,pseudo_sp)
 
-jrg_surrogs_pp_CP<-readRDS("./Results/Results_pps_exist_JRG_CommonAndPseudo/SomeSurrogates.RDS")
-dim(jrg_surrogs_pp_CP)
+#jrg_surrogs_pp_CP<-readRDS("./Results/Results_pps_exist_JRG_CommonAndPseudo/SomeSurrogates.RDS")
+#dim(jrg_surrogs_pp_CP)
 
-numsurrog<-10000
+#numsurrog<-10000
 #_________________________________________________________________
-set.seed(seed=101) # set seed to sample any surrogates randomly
+#set.seed(seed=101) # set seed to sample any surrogates randomly
 #_________________________________________________________________
-id_surrogs<-sample(c(1:dim(jrg_surrogs_pp_CP)[3]),numsurrog,replace=F)
+#id_surrogs<-sample(c(1:dim(jrg_surrogs_pp_CP)[3]),numsurrog,replace=F)
 
-jrg_surrogs_pp_CP<-jrg_surrogs_pp_CP[,,id_surrogs]
+#jrg_surrogs_pp_CP<-jrg_surrogs_pp_CP[,,id_surrogs]
 
-jrg_CP<-make_tab_stability(m=m,surrogs = jrg_surrogs_pp_CP,surrogs_given = T)
-ans<-(jrg_CP$df_stability)
-rownames(ans)<-"C+P"
-class(ans)
+#jrg_CP<-make_tab_stability(m=m,surrogs = jrg_surrogs_pp_CP,surrogs_given = T)
+#ans<-(jrg_CP$df_stability)
+#rownames(ans)<-"C+P"
+#class(ans)
 
-write.csv(ans,"./Results/jrg_results/skewness_results/jrg_stability_CP.csv")
+#write.csv(ans,"./Results/jrg_results/skewness_results/jrg_stability_CP.csv")
 
 #----------------------------------------------------
-pdf("./Results/jrg_results/skewness_results/jrg_pearson_preserving_results_cvsq_skw_plots.pdf")
-op<-par(mfrow=c(2,1))
+#pdf("./Results/jrg_results/skewness_results/jrg_pearson_preserving_results_cvsq_skw_plots.pdf")
+#op<-par(mfrow=c(2,1))
 
 #--------------CVsq histogrm-------------------------------------
-xlm<-range(ans$cvsq_real,ans$cvsq_indep,jrg_CP$cvsq_surrogs)
+#xlm<-range(ans$cvsq_real,ans$cvsq_indep,jrg_CP$cvsq_surrogs)
 
-hist(jrg_CP$cvsq_surrogs,col="grey",border=F,breaks=100,xaxt="n",xlim=xlm,xlab="10000 PP Surrogs CVsq",main="")
-axis(side=1, at=c(xlm[1],0,xlm[2]))
-abline(v=ans$cvsq_real,col="red") # actual CVsq from real data
+#hist(jrg_CP$cvsq_surrogs,col="grey",border=F,breaks=100,xaxt="n",xlim=xlm,xlab="10000 PP Surrogs CVsq",main="")
+#axis(side=1, at=c(xlm[1],0,xlm[2]))
+#abline(v=ans$cvsq_real,col="red") # actual CVsq from real data
 
 #95% quantiles
-abline(v=ans$cvsq_ntd_0.025CI,col="blue") 
-abline(v=ans$cvsq_ntd_0.975CI,col="blue")
+#abline(v=ans$cvsq_ntd_0.025CI,col="blue") 
+#abline(v=ans$cvsq_ntd_0.975CI,col="blue")
 
 #50% quantiles
-CI_cvsq_50<-quantile(jrg_CP$cvsq_surrogs,probs=c(.25,.75))
-abline(v=CI_cvsq_50[1],col="green")
-abline(v=CI_cvsq_50[2],col="green")
+#CI_cvsq_50<-quantile(jrg_CP$cvsq_surrogs,probs=c(.25,.75))
+#abline(v=CI_cvsq_50[1],col="green")
+#abline(v=CI_cvsq_50[2],col="green")
 
 # Cvsq with no tail dep.
-abline(v=ans$cvsq_ntd_median,col="magenta")
+#abline(v=ans$cvsq_ntd_median,col="magenta")
 
 # Cvsq if indep.
-abline(v=ans$cvsq_indep,col="black")
+#abline(v=ans$cvsq_indep,col="black")
 
 
 #--------------skw histogrm-------------------------------------
-xlm<-range(ans$skw_real,ans$skw_indep,jrg_CP$skw_surrogs)
+#xlm<-range(ans$skw_real,ans$skw_indep,jrg_CP$skw_surrogs)
 
-hist(jrg_CP$skw_surrogs,col="grey",border=F,breaks=100,xaxt="n",xlim=xlm,xlab="10000 PP Surrogs Skewness",main="")
-axis(side=1, at=c(xlm[1],0,xlm[2]))
-abline(v=ans$skw_real,col="red") # actual CVsq from real data
+#hist(jrg_CP$skw_surrogs,col="grey",border=F,breaks=100,xaxt="n",xlim=xlm,xlab="10000 PP Surrogs Skewness",main="")
+#axis(side=1, at=c(xlm[1],0,xlm[2]))
+#abline(v=ans$skw_real,col="red") # actual CVsq from real data
 
 #95% quantiles
-abline(v=ans$skw_ntd_0.025CI,col="blue") 
-abline(v=ans$skw_ntd_0.975CI,col="blue")
+#abline(v=ans$skw_ntd_0.025CI,col="blue") 
+#abline(v=ans$skw_ntd_0.975CI,col="blue")
 
 #50% quantiles
-CI_skw_50<-quantile(jrg_CP$skw_surrogs,probs=c(.25,.75))
-abline(v=CI_skw_50[1],col="green")
-abline(v=CI_skw_50[2],col="green")
+#CI_skw_50<-quantile(jrg_CP$skw_surrogs,probs=c(.25,.75))
+#abline(v=CI_skw_50[1],col="green")
+#abline(v=CI_skw_50[2],col="green")
 
 # Skewness with no tail dep.
-abline(v=ans$skw_ntd_median,col="magenta")
+#abline(v=ans$skw_ntd_median,col="magenta")
 
 # Skewness if indep.
-abline(v=ans$skw_indep,col="black")
+#abline(v=ans$skw_indep,col="black")
 
 # add legend
-legend("topright",col=c("red","magenta","blue","green","black"),lty=c(1,1,1,1),
-       horiz = F, bty="n",
-       legend=c("real value","no Tail-dep. (median)","95%CI","50%CI","Indep."))
+#legend("topright",col=c("red","magenta","blue","green","black"),lty=c(1,1,1,1),
+#       horiz = F, bty="n",
+#       legend=c("real value","no Tail-dep. (median)","95%CI","50%CI","Indep."))
 
 
-par(op)
-dev.off()
+#par(op)
+#dev.off()
 #======================================================================================================
 
-# =============================================Now call for hays data=============================
+# =============================================Now call for hays data========================================================
+d_hays<- readRDS("./Results/hays_results/skewness_results/ts_mat_all_sp_146_hays.RDS")# for all species except in "REMOVE" category
+category_hays<-readRDS("./Results/hays_results/skewness_results/all_sp_146_hays_category.RDS")  
+
 m<-d_hays
 id_sp<-which(category_hays$category %in% "C")
 hays_C<-make_tab_stability(m=m,surrogs = NA,surrogs_given = F)
@@ -308,8 +309,12 @@ legend("topright",col=c("red","magenta","blue","green","black"),lty=c(1,1,1,1),
 par(op)
 dev.off()
 
+#============================================================================================
 
+#================================ FOR KNZ dataset =====================================
 
+d_knz<-readRDS("./Results/knz_results/ts_knz_CP.RDS")
+knz_CP<-make_tab_stability(m=d_knz,surrogs = NA,surrogs_given = F)
 
 
 
