e01126ea782479723dbd62a12310a299f9065792
sghosh89 <ghoshshyamolina89@gmail.com>
2019-07-29 23:14:12 -0500

    code changed to plot cor stat result only, bug fixed in NonParamstat.R to get correct spearman not pearson correlation

--- NonParamStat.R
+++ NonParamStat.R
@@ -1,29 +1,26 @@
 # ------------------------------------------------------------------------------------------------
-# THIS CODE IS WRITTEN TO TEST NON-PARAMETRIC STATS FOR ANY TWO POSITIVELY CORRELATED SPECIES PAIR
-#    ..............to maintain consistency with model selection approach.
+# THIS CODE IS WRITTEN TO TEST NON-PARAMETRIC STATS based on partial Spearmn correlation (Cor stat) 
+#              FOR ANY TWO POSITIVELY CORRELATED SPECIES PAIR
 #--------------------------------------------------------------------------------------------------
 source("CopulaFunctions.R")
 source("vivj_matrix.R")
 source("good_splist.R")
-
 #---------------------------------------------------------------
 #Processing function for a copula approach to synchrony
 #
 #---Input---------
-# m : output from vivj_matrix.R
-#
-#------Output - A list with these elements-----------
+# m : output matrix from vivj_matrix.R
+# nbin : number of bins used to get npa stats
+
+#------Output - A list with these elements-------------------------------
 # ranks :       A dataframe with 2 columns, one for 
 #               ds1 and one for ds2, corresponding to samples from the copula.
 # spear   :     Spearman correlation (single number)
 # kend      :   Kendall correlation (single number)
-# Corl,Coru  :      covariance based statistics
-# Pl,Pu    :  statistics show how the points within a copula scattered from right diagonal of the box within a pentagon
-# Tl,Tu    :  statistics show how the points within a copula scattered along right diagonal of the box within a triangle
-# D2l,D2u :   measures the squared distance between the points from the right diagonal within a copula 
+# Corl,Coru  :  covariance based statistics
 
 copsync<-function(m,nbin){
-
+  
   vi<-m[,1]
   vj<-m[,2]
   
@@ -33,63 +30,36 @@ copsync<-function(m,nbin){
   var_vi<-var(vi)
   var_vj<-var(vj)
   
+  spear<-NA
+  kend<-NA
+  Corl<-NA
+  Coru<-NA
+ 
+  
   if (length(vi)>0){
     #get spear
-    spear<-cor(vi,vj, method ="pearson") 
+    spear<-cor(vi,vj, method ="spearman") 
     
     #get kend
     kend<-cor(vi, vj, method ="kendall") 
     
     #----------------------------------------------------STATISTICS :2 ---------------------------------------------------------
     #get Corl, Coru   (covariance based new stat)
-    stat2<-CorlCoru(vi,vj,nbin=nbin)
-    Corl<-stat2[1]
-    Coru<-stat2[2]
     
-    #---------------------------------------------------------- STATISTICS : 4 -----------------------------------------------------------------
-    #   get New statistics : Pl and Pu   # distance from right diagonal in lower and upper triangle based stat
-    stat4<-PlPu(vi,vj,nbin=nbin)
-    Sl_Su_Si_P<-stat4[[1]]
-    Pl<-stat4[[2]]
-    Pu<-stat4[[3]]
-    
-    #--------------------------------------------------------- STATISTICS : 6 -----------------------------------------------------------------------
-    # get D2l : average of squared distance of points from the right diagonal of the box for lower triangle
-    # get D2u : average of squared distance of points from the right diagonal of the box for upper triangle
-    stat6<-D2lD2u(vi,vj,nbin=nbin)
-    D2l<-stat6[1]
-    D2u<-stat6[2]
-    
-  }else{
-    
-    spear<-NA
-    kend<-NA
-    Corl<-NA
-    Coru<-NA
-    Sl_Su_Si_P<-NA
-    Pl<-NA
-    Pu<-NA
-    D2l<-NA
-    D2u<-NA
-    
+   # if(npa_stats=="cor"){
+      stat2<-CorlCoru(vi,vj,nbin=nbin)
+      Corl<-stat2[1]
+      Coru<-stat2[2]
+   # }
   }
-  
-  
   return(list(ranks=data.frame(Rki=vi,Rkj=vj),
               spear=spear,kend=kend,
-              Corl=Corl,Coru=Coru,
-              Sl_Su_Si_P=Sl_Su_Si_P,Pl=Pl,Pu=Pu,
-              D2l=D2l,D2u=D2u))
+              Corl=Corl,Coru=Coru))
 }
 
 #---------------------------------------------------------------------------------------------------------------------------
 
-#---------------------------------------------------------------------------------------------------------------------------
-#Calling the above on all pairs of several time series and plotting
-#results and returning all stats. All this function does is call the 
-#above function on all pairs of time series, organize the results, 
-#and make plots.
-#
+#Calling the above on all pairs of several time series and returning npa stats
 #-------------Input---------------------------------
 # d_allsp  :    A list of data frames, each with columns Year and Dat
 #               The years are assumed to be sequential and all included,
@@ -98,25 +68,20 @@ copsync<-function(m,nbin){
 # loc  :        location number
 # pfname   :    Filename (without extension) prepended to plot files saved.
 # good_sp  :    a vector of chosen species
+# nbin : number of bins used to compute npa stats
 
-# nvar    :     an integer : the number of drivers for css # omitted at present
-# timeavg : a logical tag whether you want to use timeavg data (if TRUE) or spatialavg data (if FALSE)
+#----------Output - A list of length 1 with these elements-----------------
 
-#----------Output - A list with these elements-----------------
-# D                A matrix of geographic distances between sampling locations
 # spear            A matrix of spearman results, length(d) by length(d)
 # kend             A matrix of kendall results, length(d) by length(d)
+
 # Corl             A matrix of Cl results, length(d) by length(d)
 # Coru             A matrix of Cu results, length(d) by length(d)
-# Pl               A matrix of Shy_lt results, length(d) by length(d)
-# Pu               A matrix of Shy_ut results, length(d) by length(d)
-# D2l              A matrix of R_l results, length(d) by length(d)
-# D2u              A matrix of R_u results, length(d) by length(d)
-# numericdf        A dataframe containing all the statistical data
-# summary_nLU_npa  A dataframe counting the numbers of Corl-Coru, Pl-Pu, D2u-D2l positive/negative
-#                   only for sp-sp interaction
 
-multcall<-function(d_allsp,loc,pfname,good_sp,nbin,timeavg){
+#posnI             A matrix for indices of indep. sp. pair
+#posnN             A matrix for indices of negatively correlated sp. pair
+
+multcall<-function(d_allsp,loc,resloc,good_sp,nbin){
   
   lensp<-length(good_sp)
   
@@ -137,25 +102,11 @@ multcall<-function(d_allsp,loc,pfname,good_sp,nbin,timeavg){
   colnames(Coru) <- colnames(spear)
   rownames(Coru) <-rownames(spear)
   
-  Pl<-matrix(NA,lensp,lensp)
-  colnames(Pl) <- colnames(spear)
-  rownames(Pl) <-rownames(spear)
-  
-  Pu<-matrix(NA,lensp,lensp)
-  colnames(Pu) <- colnames(spear)
-  rownames(Pu) <-rownames(spear)
-  
-  D2l<-matrix(NA,lensp,lensp)
-  colnames(D2l) <- colnames(spear)
-  rownames(D2l) <-rownames(spear)
-  
-  D2u<-matrix(NA,lensp,lensp)
-  colnames(D2u) <- colnames(spear)
-  rownames(D2u) <-rownames(spear)
-  
+  corval<-spear
+  pval_BiCopIndep<-spear
   
   #------------------- PLOT :  copula_for_all_sp pair ----------------
-  pdf(paste(pfname,"_AllCops.pdf",sep=""),width=6*lensp, height=6*lensp)
+  pdf(paste(resloc,"AllCops.pdf",sep=""),width=6*lensp, height=6*lensp)
   op<-par(mfrow=c(lensp,lensp),mar=c(3,3,3,3), mgp=c(1.5,0.5,0))
   
   for (ii in c(1:lensp)){
@@ -166,8 +117,12 @@ multcall<-function(d_allsp,loc,pfname,good_sp,nbin,timeavg){
       #cat("i,j",i,j,"\n")
       
       #if(i!=j){
-      ms<-vivj_matrix(d_allsp=d_allsp,loc=loc,i=i,j=j,level=0.05,ploton=F,timeavg=timeavg,tagon=F)
+      ms<-vivj_matrix(d_allsp=d_allsp,loc=loc,i=i,j=j,level=0.05,ploton=F,onbounds=F,lb=NA,ub=NA)
       m<-ms$mat
+      
+      corval[ii,jj]<-ms$corval
+      pval_BiCopIndep[ii,jj]<-ms$IndepTestRes
+      
       thisres<-copsync(m,nbin=nbin)
       
       spear[ii,jj]<-thisres$spear
@@ -175,94 +130,32 @@ multcall<-function(d_allsp,loc,pfname,good_sp,nbin,timeavg){
       
       Corl[ii,jj]<-thisres$Corl
       Coru[ii,jj]<-thisres$Coru
-      
-      Pl[ii,jj]<-thisres$Pl
-      Pu[ii,jj]<-thisres$Pu
-      
-      D2l[ii,jj]<-thisres$D2l
-      D2u[ii,jj]<-thisres$D2u
-      
-      
-      plot(thisres$ranks$Rki,thisres$ranks$Rkj,type='p',col=rgb(0,0,0,.2),pch=19,xlim=c(0,1),ylim=c(0,1),xlab=expression(u[i]),ylab=expression(v[j]),cex.lab=2)
-      mtext(paste0("[ i, j ] ="," [",i,",",j,"] ", ","," n=",dim(thisres$ranks)[1]),side = 3, line=0.15, adj=0.5, col="red")
-    #}
-  }
- }
-  par(op)
-  dev.off()
-  
-  
-  #------------------- PLOT :  (Sl, Su and Si)_P for_all_species pair ----------------
-  pdf(paste(pfname,"_Sl_Su_Si_P.pdf",sep=""),width=6*lensp, height=6*lensp)
-  op<-par(mfrow=c(lensp,lensp),mar=c(3,3,3,3), mgp=c(1.5,0.5,0))
-  
-  for (ii in 1:lensp){
-    for (jj in 1:lensp){
-      
-      i<-good_sp[ii]
-      j<-good_sp[jj]
-      
-      #if(i!=j){
-        
-        ms<-vivj_matrix(d_allsp=d_allsp,loc=loc,i=i,j=j,level=0.05,ploton=F,timeavg=timeavg,tagon=F)
-        m<-ms$mat
-        thisres<-copsync(m,nbin=nbin)
-        
-        if(is.na(thisres$Sl_Su_Si_P$Sl_P[1]) == F){    
-          plot(thisres$Sl_Su_Si_P$dist_Sl_P,thisres$Sl_Su_Si_P$Sl_P,type='l',col="red",ylim=c(0,1),xlab=" ",ylab=" ")
-          lines(thisres$Sl_Su_Si_P$dist_Su_P,thisres$Sl_Su_Si_P$Su_P,type='l',col="blue",ylim=c(0,1))
-          lines(thisres$Sl_Su_Si_P$dist_Si_P,thisres$Sl_Su_Si_P$Si_P,type='l',lty="dashed",col="green4",ylim=c(0,1))
-          mtext(paste0("[ i, j ] ="," [",i,",",j,"], "," red : Sl_P, "," blue : Su_P, ", " green : Si_P"),side = 3, line=0.15, adj=0.5, col="black")
-        }else{
-          plot(0,0,type='p',col="white",cex=0,xlim=c(0,sqrt(2)/2),ylim=c(0,1),xlab=" ",ylab=" ")
-          mtext(paste0("[ i, j ] ="," [",i,",",j,"]," ),side = 3, line=0.15, adj=0.5, col="black")
-          mtext(paste0("NA" ),side = 3, line=-22.5, adj=0.5, col="black", cex=4)
-        }
+     
+      plot(thisres$ranks$Rki,thisres$ranks$Rkj,type='p',col=rgb(0,0,0,.2),pch=19,xlim=c(0,1),
+           ylim=c(0,1),xlab=expression(u[i]),ylab=expression(v[j]),cex.lab=2)
+      mtext(paste0("[ i, j ] ="," [",i,",",j,"] ", ","," n=",dim(thisres$ranks)[1]),
+            side = 3, line=0.15, adj=0.5, col="red")
       #}
-      
     }
   }
-  
   par(op)
   dev.off()
-  #-------------------------------------------------------------------------
-  #-------------------!!!! ATTENTION !!!!---------------------
-  #summary_nLU_npa<-matrix(NA,2,3)
-  #rownames(summary_nLU_npa)<-c("num_LT","num_UT")
-  #colnames(summary_nLU_npa)<-c("CorlmCoru","PlmPu","D2umD2l")
-  #nI<-dim(posnI_list_ln_all[[loc]])[1] #number of independence 
-  #nN<-dim(posnN_list_ln_all[[loc]])[1] #number of negcor
-  #summary_nILU_npa[1,]<-nI
-  #x<-Corl-Coru
-  #y<-x[1:(nrow(x)-nvar),1:(ncol(x)-nvar)]
-  #summary_nLU_npa[1,1]<-sum(y>0,na.rm=T)
-  #summary_nLU_npa[2,1]<-sum(y<0,na.rm=T)
   
-  #x<-Pl-Pu
-  #y<-x[1:(nrow(x)-nvar),1:(ncol(x)-nvar)]
-  #summary_nLU_npa[1,2]<-sum(y>0,na.rm=T)
-  #summary_nLU_npa[2,2]<-sum(y<0,na.rm=T)
-  
-  #x<-D2u-D2l
-  #y<-x[1:(nrow(x)-nvar),1:(ncol(x)-nvar)]
-  #summary_nLU_npa[1,3]<-sum(y>0,na.rm=T)
-  #summary_nLU_npa[2,3]<-sum(y<0,na.rm=T)
-  
-  #summary_nILU_npa[4,]<-nN
-  #----------------------------------------------------------------------
   #-------------------------------------------------------------------------
   
-  return(list(spear=spear,kend=kend,
+  level<-0.05
+  posnI<-which(pval_BiCopIndep>=level, arr.ind = T) #indices of indep. pair
+  posnN<-which(pval_BiCopIndep<level & corval <0, arr.ind = T) #indices of significantly neg. correlated pair
+  
+  #if(npa_stats=="cor"){
+    res<-list(spear=spear,kend=kend,
               Corl=Corl,Coru=Coru,
-              Pl=Pl,Pu=Pu,
-              D2l=D2l,D2u=D2u))
-}
-
-#----------------------------------------------------------------------------------------------------------------------------
-#                                                       CODE ENDS HERE
-#-----------------------------------------------------------------------------------------------------------------------------
-
-
+              posnI=posnI,
+              posnN=posnN)
+  #}
+  
+  return(res)
+} 
 
 
 
--- vivj_matrix.R
+++ vivj_matrix.R
@@ -7,67 +7,56 @@
 # i,j : sp-pair indices
 # level : significance level for BiCopIndepTest p-value
 # ploton : (optional) logical, if T gives copula plot without transforming j-th variable to it's -ve value
-# timeavg : a logical tag whether you want to use timeavg data (if TRUE) or spatialavg data (if FALSE)
-# tagon : a logical tag if you want to label each point in the plot with year (for spatialavg data) or 
-#                                       with plot-id (for timeavg data)
+# onbounds : a logical tag (default=FALSE) to get info about the points exactly lying on bounds, if set to TRUE 
+#            then the arguments lb and ub must be numeric
+# lb : numeric value between [0,1] for lower bound (default =NA)
+# ub : numeric value between [0,1] for upper bound (default =NA), ub should be greater than lb 
 
 # Output :
 # A list of 4 elements:
 #                      mat : a matrix : copula of (vi,vj) with transforming j-th variable to it's -ve value for -ve corr.
-#                      tauval : Kendall's tauvalue 
+#                      corval : Spearman's correlation
 #                      pval   : pvalue of Kendall's cor.test
 #                      IndepTestRes : BiCopIndepTest p-value
+
 # and an optional plot of the copula
 
 library(VineCopula)
-vivj_matrix<-function(d_allsp,loc,i,j,level=0.05,ploton,timeavg,tagon){
+vivj_matrix<-function(d_allsp,loc,i,j,level=0.05,ploton,onbounds=F,lb=NA,ub=NA){
   
-  if(timeavg==F){
-    ds1<-d_allsp[[loc]][[i]]
-    ds2<-d_allsp[[loc]][[j]]
-    #----------------------------
-    colnames(ds1)<-c("Year","Dat")  # ensuring column names
-    colnames(ds2)<-c("Year","Dat")
+  ds1<-d_allsp[[loc]][[i]]
+  ds2<-d_allsp[[loc]][[j]]
+  #----------------------------
+  colnames(ds1)<-c("Year","Dat")  # ensuring column names
+  colnames(ds2)<-c("Year","Dat")
+  
+  a1<-ds1$Year[1]
+  a2<-ds2$Year[1]
+  a3<-ds1$Year[dim(ds1)[1]]
+  a4<-ds2$Year[dim(ds2)[1]]
+  year_s<-max(a1,a2)
+  year_e<-min(a3,a4)
+  ind_s1<-which(ds1$Year==year_s)
+  ind_s2<-which(ds2$Year==year_s)
+  ind_e1<-which(ds1$Year==year_e)
+  ind_e2<-which(ds2$Year==year_e)
+  ds1<-ds1[ind_s1:ind_e1,]
+  ds2<-ds2[ind_s2:ind_e2,]
+  # Omitting the years and data containing NA in either d1 or d2 
+  #from both d1 and d2
+  if(anyNA(ds1$Dat)==T | anyNA(ds2$Dat)==T){
+    ind_na1<-which(is.na(ds1$Dat))
+    ind_na2<-c(ind_na1,which(is.na(ds2$Dat)))
+    ind_na<-unique(ind_na2)
     
-    a1<-ds1$Year[1]
-    a2<-ds2$Year[1]
-    a3<-ds1$Year[dim(ds1)[1]]
-    a4<-ds2$Year[dim(ds2)[1]]
-    year_s<-max(a1,a2)
-    year_e<-min(a3,a4)
-    ind_s1<-which(ds1$Year==year_s)
-    ind_s2<-which(ds2$Year==year_s)
-    ind_e1<-which(ds1$Year==year_e)
-    ind_e2<-which(ds2$Year==year_e)
-    ds1<-ds1[ind_s1:ind_e1,]
-    ds2<-ds2[ind_s2:ind_e2,]
-    # Omitting the years and data containing NA in either d1 or d2 
-    #from both d1 and d2
-    if(anyNA(ds1$Dat)==T | anyNA(ds2$Dat)==T){
-      ind_na1<-which(is.na(ds1$Dat))
-      ind_na2<-c(ind_na1,which(is.na(ds2$Dat)))
-      ind_na<-unique(ind_na2)
-      
-      d1Dat<-ds1$Dat[-ind_na]
-      d2Dat<-ds2$Dat[-ind_na]
-      Years<-ds1$Year[-ind_na]
-      d1<-data.frame(Year=Years,Dat=d1Dat)
-      d2<-data.frame(Year=Years,Dat=d2Dat)
-    } else {
-      d1<-ds1
-      d2<-ds2
-    }
+    d1Dat<-ds1$Dat[-ind_na]
+    d2Dat<-ds2$Dat[-ind_na]
+    Years<-ds1$Year[-ind_na]
+    d1<-data.frame(Year=Years,Dat=d1Dat)
+    d2<-data.frame(Year=Years,Dat=d2Dat)
   }else{
-    ds1<-d_allsp[[loc]][[i]]
-    ds2<-d_allsp[[loc]][[j]]
-    
-    stopifnot(ds1[,1]==ds2[,1])
-    z<-cbind(ds1,ds2[,2])
-    z<-na.omit(z)
-    z<-as.data.frame(z)
-    
-    d1<-z[,c(1:2)]
-    d2<-z[,c(1,3)]
+    d1<-ds1
+    d2<-ds2
   }
   
   colnames(d1)[2]<-"Dat"  # ensuring column names
@@ -78,41 +67,66 @@ vivj_matrix<-function(d_allsp,loc,i,j,level=0.05,ploton,timeavg,tagon){
   vj<-VineCopula::pobs(d2$Dat)
   
   IndepTestRes<-VineCopula::BiCopIndTest(vi,vj)$p.value
-  ct<-cor.test(vi,vj,alternative = "two.sided",method="kendall",exact=F)
-  tauval<-unname(ct$estimate)
+  ct<-cor.test(vi,vj,alternative = "two.sided",method="spearman",exact=F)
+  corval<-unname(ct$estimate)
   pval<-ct$p.value
   
   if(ploton==T){
-    if(IndepTestRes<level && tauval>0){
+    if(IndepTestRes<level && corval>0){ # for significant positive correlation
       plot(vi,vj,type='p',col=rgb(0,0,0,0.3),pch=19,xlim=c(0,1),ylim=c(0,1),
-             xlab=names(d_allsp[[loc]])[i],ylab=names(d_allsp[[loc]])[j],cex.lab=1.5)
-      if(tagon==T && timeavg==F){
-        text(vi,vj,labels = d1[,1],cex=0.5,col="blue")
-      }else if(tagon==T && timeavg==T){
-        text(vi,vj,labels = d1[,1],cex=0.5,col="blue")
-      }else{
-        points(-1,0)
+           xlab=names(d_allsp[[loc]])[i],ylab=names(d_allsp[[loc]])[j],cex.lab=1.5)
+      
+      
+      if(j>i){
+        if(onbounds==T & identical(vi,vj)==F){
+          ind_lb<-which(vi+vj==(2*lb))
+          ind_ub<-which(vi+vj==(2*ub))
+          onlb<-length(ind_lb)
+          onub<-length(ind_ub)
+          
+          if(onlb!=0 | onub!=0){
+            mtext(paste0("onbs = (",onlb," , ",onub,")"),
+                  side = 4, line=0.15, adj=0.5, col="red") 
+          }
+        }
       }
-    }else if(IndepTestRes<level && tauval<0){
+      
+      
+    }else if(IndepTestRes<level && corval<0){ # for significant negative correlation
       plot(vi,vj,type='p',col=rgb(0,1,0,0.3),pch=19,xlim=c(0,1),ylim=c(0,1),
            xlab=names(d_allsp[[loc]])[i],ylab=names(d_allsp[[loc]])[j],cex.lab=1.5)
-      if(tagon==T && timeavg==F){
-        text(vi,vj,labels = d1[,1],cex=0.5,col="blue")
-      }else if(tagon==T && timeavg==T){
-        text(vi,vj,labels = d1[,1],cex=0.5,col="blue")
-      }else{
-        points(-1,0)
+      
+      if(j>i){
+        if(onbounds==T & identical(vi,vj)==F){
+          vneg<-VineCopula::pobs(-(d2$Dat)) # see when we count points on bounds we took reverse of second variable
+          ind_lb<-which(vi+vneg==(2*lb))
+          ind_ub<-which(vi+vneg==(2*ub))
+          
+          #vneg<-VineCopula::pobs(-(d1$Dat)) # NOTE : onbs will not be same if we consider first variable to be reversed
+          #ind_lb<-which(vj+vneg==(2*lb))
+          #ind_ub<-which(vj+vneg==(2*ub))
+          
+          onlb<-length(ind_lb)
+          onub<-length(ind_ub)
+          
+          if(onlb!=0 | onub!=0){ 
+            mtext(paste0("onbs = (",onlb," , ",onub,")"),
+                  side = 4, line=0.15, adj=0.5, col="red") 
+          }
+        }
       }
-    }else{
+      
+    }else{ # independent case
       plot(-1,0,xlim=c(0,1),ylim=c(0,1),xlab=names(d_allsp[[loc]])[i],ylab=names(d_allsp[[loc]])[j],cex.lab=1.5)
       text(0.5,0.5,"Indep.",adj=c(0.5,.5),cex=2)
     }
     mtext(paste0("(sp_x, sp_y) = (",i," , ",j,")"),
           side = 3, line=0.15, adj=0.5, col="black")
+    
   }
   
-  if(IndepTestRes<level && tauval<0){
-     vj<-VineCopula::pobs(-(d2$Dat))
+  if(IndepTestRes<level && corval<0){
+    vj<-VineCopula::pobs(-(d2$Dat))
   }
   
   Years<-d1$Year
@@ -123,9 +137,10 @@ vivj_matrix<-function(d_allsp,loc,i,j,level=0.05,ploton,timeavg,tagon){
   #-------------------------
   mat<-as.matrix(cbind(vi,vj))
   return(list(mat=mat,   # return reversed mat so that if you plot this mat you get +ve correlation 
-              tauval=tauval,  # but return the actual -ve corr. value  
+              corval=corval,  # but return the actual -ve corr. value  
               pval=pval,
               IndepTestRes=IndepTestRes))  
 }
+
 
 
