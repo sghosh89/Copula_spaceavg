a7d92fb8118d1d71ae649a0a6aa19ea61ac3820e
sghosh89 <ghoshshyamolina89@gmail.com>
2020-03-06 14:16:28 -0600

    pairwise correlation comparison plot edited

--- Plotter_PPsurrogs_tests.R
+++ Plotter_PPsurrogs_tests.R
@@ -4,9 +4,10 @@ library(corrplot)
 
 getqs<-function(x){
   qs<-quantile(x,c(0.025,0.25,0.5,0.75,0.975))
-  ql<-unname(qs[2]) #25% quantile
-  qh<-unname(qs[4]) #75% quantile
-  return(c(ql,qh))
+  ql<-unname(qs[1]) #2.5% quantile
+  qm<-unname(qs[3]) #50% quantile
+  qh<-unname(qs[5]) #97.5% quantile
+  return(c(ql,qm,qh))
 }
 
 
@@ -14,9 +15,8 @@ getqs<-function(x){
 # m: a matrix with common and pseudo sp time series along each column
 # ans: a list which is the output from PPsurrogs_tests function
 # resloc: folder path to save the plots
-# ht, wd: height and width of correlation comparing plot
 
-Plotter_PPsurrogs_tests<-function(m,ans,resloc,wd=15,ht=15){
+Plotter_PPsurrogs_tests<-function(m,ans,resloc){
   
   cm<-cor(m)
   s1<-ans$cor_surrogs
@@ -30,35 +30,62 @@ Plotter_PPsurrogs_tests<-function(m,ans,resloc,wd=15,ht=15){
   
   #dim(cs)
   
-  cm_low<-cm
-  cm_high<-cm
+  cm_low<-cm_mid<-cm_up<-cm # initialize
   
   for(i in 1:nrow(cs)){
     for(j in 1:ncol(cs)){
       mm<-cs[i,j,]
       tempo<-getqs(mm)
       cm_low[i,j]<-tempo[1]
-      cm_high[i,j]<-tempo[2]
+      cm_mid[i,j]<-tempo[2]
+      cm_up[i,j]<-tempo[3]
     }
   }
+ 
+  nspair<-0.5*nrow(cm)*(nrow(cm)-1) # number of unique species pair
+  my_dat<-data.frame(real_cor=NA*numeric(nspair),
+                     surrogs_cor_lowCI=NA*numeric(nspair),
+                     surrogs_cor_midCI=NA*numeric(nspair),
+                     surrogs_cor_upCI=NA*numeric(nspair),
+                     color_code=NA*numeric(nspair)) 
   
-  #col4 <- colorRampPalette(c("#7F0000", "red", "#FF7F00", "yellow", "#7FFF7F",
-  #                           "cyan", "#007FFF", "blue", "#00007F"))
-  #corrplot(cm, type="upper",diag=F,col=col4(10))
-  #corrplot(cm, type="upper",diag=F)
+  my_dat$real_cor<-cm[lower.tri(cm)]
+  my_dat$surrogs_cor_lowCI<-cm_low[lower.tri(cm_low)]
+  my_dat$surrogs_cor_midCI<-cm_mid[lower.tri(cm_mid)]
+  my_dat$surrogs_cor_upCI<-cm_up[lower.tri(cm_up)]
   
-  col1 <- colorRampPalette(c("blue","white","red")) 
+  id<-which(my_dat$surrogs_cor_lowCI>my_dat$real_cor | my_dat$surrogs_cor_upCI<my_dat$real_cor)
   
-  pdf(paste(resloc,"./compare_pairwisecor_PPsurrogs.pdf",sep=""),width=wd,height=ht)
-  op<-par(mar=c(0.5,0.5,0.5,0.5))
-  rownames(cm)<-colnames(cm)<-paste("sp",c(1:nrow(cm)))
-  diag(cm)<-diag(cm_low)<-diag(cm_high)<-NA
-  corrplot(cm, low = cm_low, upp = cm_high, type='lower',diag=F, plotC = "rect", col=col1(100), 
-           tl.cex=2,tl.col = "black",tl.offset = 1,tl.pos="ld",addgrid.col = "grey",
-           cl.cex = 2,cl.length=7,tl.srt=20,
-           cl.align.text = "l",cl.ratio = 0.1)
+  my_dat$color_code<-rgb(0,0,0,0.3) #black
+  my_dat$color_code[id]<-rgb(1,0,0,0.6) #red
+ 
+  pdf(paste(resloc,"./comparison_pairwisecor_PPsurrogs.pdf",sep=""),height=5,width=5)
+  op<-par(mar=c(5,5,1,1),mgp=c(3,1,0))
+  
+  plot(my_dat$real_cor,my_dat$surrogs_cor_midCI,
+       xlim=c(-1,1),ylim=c(-1,1),
+       xlab="pairwise Pearson correlation from real data",ylab="pairwise Pearson correlation from surrogates",
+       cex=0.5,pch=20,col=my_dat$color_code)
+  segments(x0=-1,y0=-1,x1=1,y1=1,lty="dashed")
+  
+  segments(x0=my_dat$real_cor,y0=my_dat$surrogs_cor_lowCI,
+           x1=my_dat$real_cor,y1=my_dat$surrogs_cor_upCI,col=my_dat$color_code)
+  
   par(op)
   dev.off()
+  #-----------------------------------------------------------------------------------------------
+  #col1 <- colorRampPalette(c("blue","white","red")) 
+  #pdf(paste(resloc,"./compare_pairwisecor_PPsurrogs.pdf",sep=""),width=15,height=15)
+  #op<-par(mar=c(0.5,0.5,0.5,0.5))
+  #rownames(cm)<-colnames(cm)<-paste("sp",c(1:nrow(cm)))
+  #diag(cm)<-diag(cm_low)<-diag(cm_high)<-NA
+  #corrplot(cm, low = cm_low, upp = cm_high, type='lower',diag=F, plotC = "rect", col=col1(100), 
+  #         tl.cex=2,tl.col = "black",tl.offset = 1,tl.pos="ld",addgrid.col = "grey",
+  #         cl.cex = 2,cl.length=7,tl.srt=20,
+  #         cl.align.text = "l",cl.ratio = 0.1)
+  #par(op)
+  #dev.off()
+  #-----------------------------------------------------------------------------------------------
   
   
   pdf(paste(resloc,"./comparison_histplot_PPsurrogs.pdf",sep=""),height=7,width=9)
@@ -97,7 +124,6 @@ Plotter_PPsurrogs_tests<-function(m,ans,resloc,wd=15,ht=15){
 #m<-readRDS("./Results/hays_results/skewness_results/ts_mat_CP_hays.RDS")
 #ans<-readRDS("./Results/hays_results/skewness_results/pp_surrogs_hays_CP/PPsurrogs_tests_with_HaysSurrogates.RDS")
 #resloc<-"./Results/hays_results/skewness_results/pp_surrogs_hays_CP/"
-
 #Plotter_PPsurrogs_tests(m=m,ans=ans,resloc=resloc,ht=15,wd=15)
 
 
