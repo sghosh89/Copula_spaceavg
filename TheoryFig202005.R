7bd8625205b4796d17a9848fe2423d3693290fe2
Dan Reuman <reuman@ku.edu>
2020-05-07 16:19:02 -0500

    theory figure try 2

--- TheoryFig202005.R
+++ TheoryFig202005.R
@@ -14,7 +14,7 @@ source("SkewnessAnd3CentMom.R")
 #Args
 #corpar           For the part of the distribution where the variable are
 #                   not perfectly related they are generated with a normal
-#                   copula with this parameter
+#                   copula with this parameter in all off-diagonal locations
 #beta1, beta2     These specify where the tail dependence starts in each tail.
 #                   Numbers between 0 and 1 with beta1<beta2.
 #numpts           Number of draws from the copula desired
@@ -51,48 +51,42 @@ plot(res[,2],res[,3],type="p",pch=20,cex=.5)
 
 numpts<-10000
 numscatpts<-500
+dtouse<-20
 
-#Functions which when applied to uniformly distributed data on (0,1) give X and Y.
-#To be used to establish the marginals in all 5 examples
-#margXfunc<-function(c){qgamma(c,shape=2,scale=2)}
-#margYfunc<-function(c){qgamma(c,shape=7.5,scale=1)}
-margXfunc<-function(c){qgamma(c,shape=9,scale=.5)}
-margYfunc<-function(c){qgamma(c,shape=7.5,scale=1)}
-#margXfunc<-function(c){qnorm(c)}
-#margYfunc<-function(c){qnorm(c)}
+#Functions which control the marginals
+#margfunc<-function(c){qgamma(c,shape=2,scale=2)}
+#margfunc<-function(c){qgamma(c,shape=7.5,scale=1)}
+#margfunc<-function(c){qgamma(c,shape=9,scale=.5)}
+margfunc<-function(c){qnorm(c)}
 
 #get data for example 1 - normal copula case
-sig<-matrix(c(1,.6,.6,1),2,2)
-cop1<-normalCopula(param=P2p(sig),dim=2,dispstr="un")
-dcop1<-rCopula(numpts,cop1)
-d1<-dcop1
-d1[,1]<-margXfunc(d1[,1])
-d1[,2]<-margYfunc(d1[,2])
-cov1<-cov(d1)[1,2]
-XpY1<-d1[,1]+d1[,2]
-varXpY1<-var(XpY1)
-sk1<-myskns(XpY1)
+dcop1<-taildepcopdat(.6,0,1,numpts,dtouse)
+d1<-margfunc(dcop1)
+cov1<-cov(d1)
+diag(cov1)<-NA
+cov1<-mean(cov1,na.rm=TRUE)
+totX1<-apply(FUN=sum,X=d1,MARGIN=1)
+vartotX1<-var(totX1)
+sktotX1<-myskns(totX1)
+empcdf1<-data.frame(x=sort(totX1),(1:numpts)/numpts)
 
 #get data for example 2 - comonotonic
-dcop2<-matrix(rep(runif(numpts),times=2),numpts,2)
-d2<-dcop2
-d2[,1]<-margXfunc(d2[,1])
-d2[,2]<-margYfunc(d2[,2])
-cov2<-cov(d2)[1,2]
-XpY2<-d2[,1]+d2[,2]
-varXpY2<-var(XpY2)
-sk2<-myskns(XpY2)
+dcop2<-matrix(rep(runif(numpts),times=dtouse),numpts,dtouse)
+d2<-margfunc(dcop2)
+cov2<-cov(d2)
+diag(cov2)<-NA
+cov2<-mean(cov2,na.rm=TRUE)
+totX2<-apply(FUN=sum,X=d2,MARGIN=1)
+vartotX2<-var(totX2)
+sktotX2<-myskns(totX2)
+empcdf2<-data.frame(x=sort(totX2),(1:numpts)/numpts)
 
 #get data for example 3 - upper-tail comonotonic
-d3<-matrix(NA,numpts,2)
 beta1<-0
 beta2<-0.95
 objfun<-function(rho)
 {
-  cdat<-taildepcopdat(rho,beta1,beta2,numpts)
-  dat<-cdat
-  dat[,1]<-margXfunc(dat[,1])
-  dat[,2]<-margYfunc(dat[,2])
+  dat<-margfunc(taildepcopdat(rho,beta1,beta2,numpts))
   h<-cov(dat)[1,2]
   return(c(h,(h-cov1)^2))
 }
@@ -113,17 +107,17 @@ mod<-lm(ycov~x)
 lines(range(x),coef(mod)[1]+coef(mod)[2]*range(x),type="l")
 parmtouse<-(cov1-coef(mod)[1])/coef(mod)[2]
 lines(rep(parmtouse,2),range(ycov),type="l")
-dcop3<-taildepcopdat(parmtouse,beta1,beta2,numpts)
-d3<-dcop3
-d3[,1]<-margXfunc(d3[,1])
-d3[,2]<-margYfunc(d3[,2])
-cov3<-cov(d3)[1,2]
-XpY3<-d3[,1]+d3[,2]
-varXpY3<-var(XpY3)
-sk3<-myskns(XpY3)
+dcop3<-taildepcopdat(parmtouse,beta1,beta2,numpts,dtouse)
+d3<-margfunc(dcop3)
+cov3<-cov(d3)
+diag(cov3)<-NA
+cov3<-mean(cov3,na.rm=TRUE)
+totX3<-apply(FUN=sum,X=d3,MARGIN=1)
+vartotX3<-var(totX3)
+sktotX3<-myskns(totX3)
+empcdf3<-data.frame(x=sort(totX3),(1:numpts)/numpts)
 
 #get data for example 4 - lower-tail comonotonic
-d4<-matrix(NA,numpts,2)
 beta1<-0.05
 beta2<-1
 x<-seq(from=.01,to=0.99,by=0.01)
@@ -143,17 +137,17 @@ mod<-lm(ycov~x)
 lines(range(x),coef(mod)[1]+coef(mod)[2]*range(x),type="l")
 parmtouse<-(cov1-coef(mod)[1])/coef(mod)[2]
 lines(rep(parmtouse,2),range(ycov),type="l")
-dcop4<-taildepcopdat(parmtouse,beta1,beta2,numpts)
-d4<-dcop4
-d4[,1]<-margXfunc(d4[,1])
-d4[,2]<-margYfunc(d4[,2])
-cov4<-cov(d4)[1,2]
-XpY4<-d4[,1]+d4[,2]
-varXpY4<-var(XpY4)
-sk4<-myskns(XpY4)
+dcop4<-taildepcopdat(parmtouse,beta1,beta2,numpts,dtouse)
+d4<-margfunc(dcop4)
+cov4<-cov(d4)
+diag(cov4)<-NA
+cov4<-mean(cov4,na.rm=TRUE)
+totX4<-apply(FUN=sum,X=d4,MARGIN=1)
+vartotX4<-var(totX4)
+sktotX4<-myskns(totX4)
+empcdf4<-data.frame(x=sort(totX4),(1:numpts)/numpts)
 
 #get data for example 5
-d5<-matrix(NA,numpts,2)
 beta1<-0.05
 beta2<-0.95
 x<-seq(from=.01,to=0.99,by=0.01)
@@ -173,29 +167,30 @@ mod<-lm(ycov~x)
 lines(range(x),coef(mod)[1]+coef(mod)[2]*range(x),type="l")
 parmtouse<-(cov1-coef(mod)[1])/coef(mod)[2]
 lines(rep(parmtouse,2),range(ycov),type="l")
-dcop5<-taildepcopdat(parmtouse,beta1,beta2,numpts)
-d5<-dcop5
-d5[,1]<-margXfunc(d5[,1])
-d5[,2]<-margYfunc(d5[,2])
-cov5<-cov(d5)[1,2]
-XpY5<-d5[,1]+d5[,2]
-varXpY5<-var(XpY5)
-sk5<-myskns(XpY5)
+dcop5<-taildepcopdat(parmtouse,beta1,beta2,numpts,dtouse)
+d5<-margfunc(dcop5)
+cov5<-cov(d5)
+diag(cov5)<-NA
+cov5<-mean(cov5,na.rm=TRUE)
+totX5<-apply(FUN=sum,X=d5,MARGIN=1)
+vartotX5<-var(totX5)
+sktotX5<-myskns(totX5)
+empcdf5<-data.frame(x=sort(totX5),(1:numpts)/numpts)
 
 #other prep for consistency among plots
 scatylim<-range(d1[,2],d2[,2],d3[,2],d4[,2],d5[,2],na.rm=TRUE)
 scatxlim<-range(d1[,1],d2[,1],d3[,1],d4[,1],d5[,1],na.rm=TRUE)
-histXbreaks<-seq(from=floor(scatxlim[1]),to=ceiling(scatxlim[2]),by=0.5)
-histYbreaks<-seq(from=floor(scatylim[1]),to=ceiling(scatylim[2]),by=0.5)
-histXpYxlim<-range(XpY1,XpY2,XpY3,XpY4,XpY5,na.rm=TRUE)
-histXpYbreaks<-seq(from=floor(histXpYxlim[1]),to=ceiling(histXpYxlim[2]),by=1)
+histXbreaks<-seq(from=scatxlim[1],to=scatxlim[2],length.out=50)
+histYbreaks<-seq(from=scatylim[1],to=scatylim[2],length.out=50)
+histtotXxlim<-range(totX1,totX2,totX3,totX4,totX5,na.rm=TRUE)
+histtotXbreaks<-seq(from=histtotXxlim[1],to=histtotXxlim[2],length.out=50)
 
 #***plot dimensions, units inches
 xmarght<-.5
-ymargwd<-.5
-totwd<-3.5
-gap<-0.15
-spf<-0.4
+ymargwd<-.6
+totwd<-4
+gap<-0.25
+spf<-0.45
 panwd<-(totwd-2*gap-2*ymargwd)/(2+spf)
 smallpan<-panwd*spf
 panht<-panwd
@@ -211,12 +206,12 @@ par(fig=c((ymargwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25)
-plot(d1[,1],d1[,2],xaxt='n',pch=20,cex=.5,col=rgb(red=190/256,green=190/256,blue=190/256,alpha=.3),
+plot(d1[1:numscatpts,1],d1[1:numscatpts,2],xaxt='n',pch=20,cex=.5,col=rgb(red=190/256,green=190/256,blue=190/256,alpha=.3),
      xlim=scatxlim,ylim=scatylim)
 axis(side=1,labels=FALSE)
-mtext("Y",side=2,line=1.2)
+mtext(expression(X[2]),side=2,line=1.2)
 
-#X marginal of scatterplot
+#X1 marginal of scatterplot
 par(fig=c((ymargwd)/totwd,
           (ymargwd+panwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht+gap)/totht,
@@ -225,11 +220,12 @@ par(fig=c((ymargwd)/totwd,
 h<-hist(d1[,1],breaks=histXbreaks,plot=FALSE)
 x<-h$breaks
 x<-x[2:length(x)]-diff(x)[1]/2
-plot(x,h$counts,xaxt='n',type="l")
+plot(x,h$counts,xaxt='n',yaxt='n',type="l")
 axis(side=1,labels=FALSE)
+axis(side=2,labels=TRUE)
 mtext("Ct.",side=2,line=1.2)
 
-#Y marginal of scatterplot
+#X2 marginal of scatterplot
 par(fig=c((ymargwd+panwd+gap)/totwd,
           (ymargwd+panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
@@ -238,25 +234,26 @@ par(fig=c((ymargwd+panwd+gap)/totwd,
 h<-hist(d1[,2],breaks=histYbreaks,plot=FALSE)
 y<-h$breaks
 y<-y[2:length(y)]-diff(y)[1]/2
-plot(h$counts,y,yaxt='n',type="l")
+plot(h$counts,y,yaxt='n',xaxt='n',type="l")
+#axis(side=1,at=c(0,400),labels=c("0","400"))
+axis(side=1,labels=TRUE)
 axis(side=2,labels=FALSE)
-#mtext("Ct.",side=1,line=1.2)
 
-#Histogram of X+Y
+#Histogram of totX
 par(fig=c((2*ymargwd+panwd+gap+smallpan)/totwd,
           (2*ymargwd+2*panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-h1<-hist(XpY1,breaks=histXpYbreaks,plot=FALSE)
+h1<-hist(totX1,breaks=histtotXbreaks,plot=FALSE)
 x1<-h1$breaks
 x1<-x1[2:length(x1)]-diff(x1)[1]/2
 plot(x1,h1$counts,xaxt='n',type="l")
 axis(side=1,labels=FALSE)
 mtext("Count",side=2,line=1.2)
-mtext(paste0("cov(X,Y)=",round(cov1,3)),side=3,line=1.6,cex=.75) #adjust the horizontal position,
-mtext(paste0("var(X+Y)=",round(varXpY1,3)),side=3,line=.8,cex=.75) #adjust the horizontal position
-mtext(paste0("sk(X+Y)=",round(sk1,3)),side=3,line=0,cex=.75) #adjust the horizontal position
+mtext(bquote(cov(X[i],X[j]) %~~% .(round(cov1,3))),side=3,line=1.6,cex=.75)
+mtext(bquote(sd(Sigma[i] * X[i]) == .(round(sqrt(vartotX1),3))),side=3,line=.8,cex=.75)
+mtext(bquote(sk(Sigma[i] * X[i]) == .(round(sktotX1,3))),side=3,line=0,cex=.75)
 
 #***Example 2, second row of panels, comonotonic case
 
@@ -267,12 +264,12 @@ par(fig=c((ymargwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-plot(d2[,1],d2[,2],xaxt='n',pch=20,cex=.5,col=rgb(red=255/256,green=0/256,blue=0/256,alpha=.3),
+plot(d2[1:numscatpts,1],d2[1:numscatpts,2],xaxt='n',pch=20,cex=.5,col=rgb(red=255/256,green=0/256,blue=0/256,alpha=.3),
      xlim=scatxlim,ylim=scatylim)
 axis(side=1,labels=FALSE)
-mtext("Y",side=2,line=1.2)
+mtext(expression(X[2]),side=2,line=1.2)
 
-#X marginal of scatterplot
+#X1 marginal of scatterplot
 par(fig=c((ymargwd)/totwd,
           (ymargwd+panwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht+gap)/totht,
@@ -285,7 +282,7 @@ plot(x,h$counts,xaxt='n',type="l")
 axis(side=1,labels=FALSE)
 mtext("Ct.",side=2,line=1.2)
 
-#Y marginal of scatterplot
+#X2 marginal of scatterplot
 par(fig=c((ymargwd+panwd+gap)/totwd,
           (ymargwd+panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
@@ -294,26 +291,26 @@ par(fig=c((ymargwd+panwd+gap)/totwd,
 h<-hist(d2[,2],breaks=histYbreaks,plot=FALSE)
 y<-h$breaks
 y<-y[2:length(y)]-diff(y)[1]/2
-plot(h$counts,y,yaxt='n',type="l")
+plot(h$counts,y,yaxt='n',xaxt='n',type="l")
+axis(side=1,labels=TRUE)
 axis(side=2,labels=FALSE)
-#mtext("Ct.",side=1,line=1.2)
 
-#Histogram of X+Y
+#Histogram of totX
 par(fig=c((2*ymargwd+panwd+gap+smallpan)/totwd,
           (2*ymargwd+2*panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-h<-hist(XpY2,breaks=histXpYbreaks,plot=FALSE)
+h<-hist(totX2,breaks=histtotXbreaks,plot=FALSE)
 x<-h$breaks
 x<-x[2:length(x)]-diff(x)[1]/2
 plot(x,h$counts,xaxt='n',type="l",col="red",ylim=range(h$counts,h1$counts))
 lines(x1,h1$counts,xaxt='n',type="l")
 axis(side=1,labels=FALSE)
 mtext("Count",side=2,line=1.2)
-mtext(paste0("cov(X,Y)=",round(cov2,3)),side=3,line=1.6,cex=.75) #adjust the horizontal position,
-mtext(paste0("var(X+Y)=",round(varXpY2,3)),side=3,line=.8,cex=.75) #adjust the horizontal position
-mtext(paste0("sk(X+Y)=",round(sk2,3)),side=3,line=0,cex=.75) #adjust the horizontal position
+mtext(bquote(cov(X[i],X[j]) %~~% .(round(cov2,3))),side=3,line=1.6,cex=.75)
+mtext(bquote(sd(Sigma[i] * X[i]) == .(round(sqrt(vartotX2),3))),side=3,line=.8,cex=.75)
+mtext(bquote(sk(Sigma[i] * X[i]) == .(round(sktotX2,3))),side=3,line=0,cex=.75)
 
 #***Example 3, third row of panels, comonotonicity in the right tails only
 
@@ -324,12 +321,12 @@ par(fig=c((ymargwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-plot(d3[,1],d3[,2],xaxt='n',pch=20,cex=.5,col=rgb(red=0/256,green=0/256,blue=255/256,alpha=.3),
+plot(d3[1:numscatpts,1],d3[1:numscatpts,2],xaxt='n',pch=20,cex=.5,col=rgb(red=0/256,green=0/256,blue=255/256,alpha=.3),
      xlim=scatxlim,ylim=scatylim)
 axis(side=1,labels=FALSE)
-mtext("Y",side=2,line=1.2)
+mtext(expression(X[2]),side=2,line=1.2)
 
-#X marginal of scatterplot
+#X1 marginal of scatterplot
 par(fig=c((ymargwd)/totwd,
           (ymargwd+panwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht+gap)/totht,
@@ -342,7 +339,7 @@ plot(x,h$counts,xaxt='n',type="l")
 axis(side=1,labels=FALSE)
 mtext("Ct.",side=2,line=1.2)
 
-#Y marginal of scatterplot
+#X2 marginal of scatterplot
 par(fig=c((ymargwd+panwd+gap)/totwd,
           (ymargwd+panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
@@ -351,26 +348,26 @@ par(fig=c((ymargwd+panwd+gap)/totwd,
 h<-hist(d3[,2],breaks=histYbreaks,plot=FALSE)
 y<-h$breaks
 y<-y[2:length(y)]-diff(y)[1]/2
-plot(h$counts,y,yaxt='n',type="l")
+plot(h$counts,y,yaxt='n',xaxt='n',type="l")
+axis(side=1,labels=TRUE)
 axis(side=2,labels=FALSE)
-#mtext("Ct.",side=1,line=1.2)
 
-#Histogram of X+Y
+#Histogram of totX
 par(fig=c((2*ymargwd+panwd+gap+smallpan)/totwd,
           (2*ymargwd+2*panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-h<-hist(XpY3,breaks=histXpYbreaks,plot=FALSE)
+h<-hist(totX3,breaks=histtotXbreaks,plot=FALSE)
 x<-h$breaks
 x<-x[2:length(x)]-diff(x)[1]/2
 plot(x,h$counts,xaxt='n',type="l",col="blue",ylim=range(h$counts,h1$counts))
 lines(x1,h1$counts,xaxt='n',type="l")
 axis(side=1,labels=FALSE)
 mtext("Count",side=2,line=1.2)
-mtext(paste0("cov(X,Y)=",round(cov3,3)),side=3,line=1.6,cex=.75) #adjust the horizontal position,
-mtext(paste0("var(X+Y)=",round(varXpY3,3)),side=3,line=.8,cex=.75) #adjust the horizontal position
-mtext(paste0("sk(X+Y)=",round(sk3,3)),side=3,line=0,cex=.75) #adjust the horizontal position
+mtext(bquote(cov(X[i],X[j]) %~~% .(round(cov3,3))),side=3,line=1.6,cex=.75)
+mtext(bquote(sd(Sigma[i] * X[i]) == .(round(sqrt(vartotX3),3))),side=3,line=.8,cex=.75)
+mtext(bquote(sk(Sigma[i] * X[i]) == .(round(sktotX3,3))),side=3,line=0,cex=.75)
 
 #***Example 4, fourth row of panels, comonotonicity in the left tails only
 
@@ -381,12 +378,12 @@ par(fig=c((ymargwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-plot(d4[,1],d4[,2],xaxt='n',pch=20,cex=.5,col=rgb(red=0/256,green=255/256,blue=0/256,alpha=.3),
+plot(d4[1:numscatpts,1],d4[1:numscatpts,2],xaxt='n',pch=20,cex=.5,col=rgb(red=0/256,green=255/256,blue=0/256,alpha=.3),
      xlim=scatxlim,ylim=scatylim)
 axis(side=1,labels=FALSE)
-mtext("Y",side=2,line=1.2)
+mtext(expression(X[2]),side=2,line=1.2)
 
-#X marginal of scatterplot
+#X1 marginal of scatterplot
 par(fig=c((ymargwd)/totwd,
           (ymargwd+panwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht+gap)/totht,
@@ -399,7 +396,7 @@ plot(x,h$counts,xaxt='n',type="l")
 axis(side=1,labels=FALSE)
 mtext("Ct.",side=2,line=1.2)
 
-#Y marginal of scatterplot
+#X2 marginal of scatterplot
 par(fig=c((ymargwd+panwd+gap)/totwd,
           (ymargwd+panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
@@ -408,9 +405,9 @@ par(fig=c((ymargwd+panwd+gap)/totwd,
 h<-hist(d4[,2],breaks=histYbreaks,plot=FALSE)
 y<-h$breaks
 y<-y[2:length(y)]-diff(y)[1]/2
-plot(h$counts,y,yaxt='n',type="l")
+plot(h$counts,y,yaxt='n',xaxt='n',type="l")
+axis(side=1,labels=TRUE)
 axis(side=2,labels=FALSE)
-#mtext("Ct.",side=1,line=1.2)
 
 #Histogram of X+Y
 par(fig=c((2*ymargwd+panwd+gap+smallpan)/totwd,
@@ -418,16 +415,16 @@ par(fig=c((2*ymargwd+panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-h<-hist(XpY4,breaks=histXpYbreaks,plot=FALSE)
+h<-hist(totX4,breaks=histtotXbreaks,plot=FALSE)
 x<-h$breaks
 x<-x[2:length(x)]-diff(x)[1]/2
 plot(x,h$counts,xaxt='n',type="l",col="green",ylim=range(h$counts,h1$counts))
 lines(x1,h1$counts,xaxt='n',type="l")
 axis(side=1,labels=FALSE)
 mtext("Count",side=2,line=1.2)
-mtext(paste0("cov(X,Y)=",round(cov4,3)),side=3,line=1.6,cex=.75) #adjust the horizontal position, fill in number
-mtext(paste0("var(X+Y)=",round(varXpY4,3)),side=3,line=.8,cex=.75) #adjust the horizontal position, fill in number
-mtext(paste0("sk(X+Y)=",round(sk4,3)),side=3,line=0,cex=.75) #adjust the horizontal position, fill in number
+mtext(bquote(cov(X[i],X[j]) %~~% .(round(cov4,3))),side=3,line=1.6,cex=.75)
+mtext(bquote(sd(Sigma[i] * X[i]) == .(round(sqrt(vartotX4),3))),side=3,line=.8,cex=.75)
+mtext(bquote(sk(Sigma[i] * X[i]) == .(round(sktotX4,3))),side=3,line=0,cex=.75)
 
 #***Example 5, fifth row of panels, comonotonicity in the right and left tails only
 
@@ -438,13 +435,13 @@ par(fig=c((ymargwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-plot(d5[,1],d5[,2],xaxt='n',pch=20,cex=.5,col=rgb(red=160/256,green=32/256,blue=240/256,alpha=.3),
+plot(d5[1:numscatpts,1],d5[1:numscatpts,2],xaxt='n',pch=20,cex=.5,col=rgb(red=160/256,green=32/256,blue=240/256,alpha=.3),
      xlim=scatxlim,ylim=scatylim)
-axis(side=1,labels=FALSE)
-mtext("Y",side=2,line=1.2)
-mtext("X",side=1,line=1.2)
+axis(side=1,labels=TRUE)
+mtext(expression(X[2]),side=2,line=1.2)
+mtext(expression(X[1]),side=1,line=1.2)
 
-#X marginal of scatterplot
+#X1 marginal of scatterplot
 par(fig=c((ymargwd)/totwd,
           (ymargwd+panwd)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht+gap)/totht,
@@ -457,7 +454,7 @@ plot(x,h$counts,xaxt='n',type="l")
 axis(side=1,labels=FALSE)
 mtext("Ct.",side=2,line=1.2)
 
-#Y marginal of scatterplot
+#X2 marginal of scatterplot
 par(fig=c((ymargwd+panwd+gap)/totwd,
           (ymargwd+panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
@@ -466,7 +463,8 @@ par(fig=c((ymargwd+panwd+gap)/totwd,
 h<-hist(d5[,2],breaks=histYbreaks,plot=FALSE)
 y<-h$breaks
 y<-y[2:length(y)]-diff(y)[1]/2
-plot(h$counts,y,yaxt='n',type="l")
+plot(h$counts,y,yaxt='n',xaxt='n',type="l")
+axis(side=1,labels=TRUE)
 axis(side=2,labels=FALSE)
 mtext("Ct.",side=1,line=1.2)
 
@@ -476,16 +474,16 @@ par(fig=c((2*ymargwd+panwd+gap+smallpan)/totwd,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan))/totht,
           (xmarght+(panrownum-1)*(panht+2*gap+smallpan)+panht)/totht),
     mai=c(0,0,0,0),mgp=c(3,.15,0),tcl=-.25,new=TRUE)
-h<-hist(XpY5,breaks=histXpYbreaks,plot=FALSE)
+h<-hist(totX5,breaks=histtotXbreaks,plot=FALSE)
 x<-h$breaks
 x<-x[2:length(x)]-diff(x)[1]/2
 plot(x,h$counts,xaxt='n',type="l",col="purple",ylim=range(h$counts,h1$counts))
 lines(x1,h1$counts,xaxt='n',type="l")
-axis(side=1,labels=FALSE)
+axis(side=1,labels=TRUE)
 mtext("Count",side=2,line=1.2)
-mtext(paste0("cov(X,Y)=",round(cov5,3)),side=3,line=1.6,cex=.75) #adjust the horizontal position, fill in number
-mtext(paste0("var(X+Y)=",round(varXpY5,3)),side=3,line=.8,cex=.75) #adjust the horizontal position, fill in number
-mtext(paste0("sk(X+Y)=",round(sk5,3)),side=3,line=0,cex=.75) #adjust the horizontal position, fill in number
-mtext("X+Y",side=1,line=1.2)
+mtext(bquote(cov(X[i],X[j]) %~~% .(round(cov5,3))),side=3,line=1.6,cex=.75)
+mtext(bquote(sd(Sigma[i] * X[i]) == .(round(sqrt(vartotX5),3))),side=3,line=.8,cex=.75)
+mtext(bquote(sk(Sigma[i] * X[i]) == .(round(sktotX5,3))),side=3,line=0,cex=.75)
+mtext(expression(Sigma[i] * X[i]),side=1,line=1.2)
 
 dev.off()
